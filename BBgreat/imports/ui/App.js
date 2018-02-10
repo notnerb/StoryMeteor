5bf567966c2ed9386a25c9da542b836e3a714875
notnerb <notnerb.94@ufl.edu>
2018-02-09 23:20:23 -0500

    mongo connected

--- BBgreat/imports/ui/App.js
+++ BBgreat/imports/ui/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
- 
+
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.js';
- 
+
 // App component - represents the whole app
-export default class App extends Component {
+ class App extends Component {
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
- 
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
         </header>
- 
+
         <ul>
           {this.renderTasks()}
         </ul>
@@ -28,8 +28,7 @@ export default class App extends Component {
   }
 }
 
- 
-export default withTracker(() => {
+ export default withTracker(() => {
   return {
     tasks: Tasks.find({}).fetch(),
   };
