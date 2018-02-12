541c2bf91bebddc2f23228d7d0471d6a363f7d5c
notnerb <notnerb.94@ufl.edu>
2018-02-12 02:00:53 -0500

    Making Some headway, exploring angular

--- BBgreat/imports/ui/App.js
+++ BBgreat/imports/ui/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-
+import ReactDOM from 'react-dom';
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.js';
 
 // App component - represents the whole app
  class App extends Component {
+   handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form. wow
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
   renderTasks() {
     return this.props.tasks2.map(/* cast to a task*/(task) => (
       //map to a Task component key and its associated task prop.
@@ -19,6 +33,14 @@ import Task from './Task.js';
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className= "new-task" onSubmit= {this.handleSubmit.bind(this)}>
+            <input
+              type = "text"
+              //reference to be stored in this.refs.name for later use
+              ref= "textInput"
+              placeholder = "Type to add new tasks"
+            />
         </header>
 
         <ul>
@@ -31,7 +53,8 @@ import Task from './Task.js';
 
  export default withTracker(() => {
   return {
-    //tasks prop??
-    tasks2: Tasks.find({}).fetch()
+    //tasks prop, indeed the prop is named here, also the find function
+    //seems to have brackets to represent bins for Mongo tuples.
+    tasks2: Tasks.find({}, {sort: { createdAt: -1} }).fetch()
   };
 })(App);
